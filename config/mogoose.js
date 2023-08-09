d41a1d2863905b954b4c389a1ac6bb5ebbda6ed3
Aman Malviya <94959490+AmanMalviya22@users.noreply.github.com>
2023-08-09 16:09:43 +0530

    Update mogoose.js

--- config/mogoose.js
+++ config/mogoose.js
@@ -2,9 +2,9 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: 'config/.env' });
+require("dotenv").config();
 //connect to the database
-mongoose.connect(`mongodb://127.0.0.1:27017/review-development`, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
